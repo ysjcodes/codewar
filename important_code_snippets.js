4c4612a4fe985c2037cb085eeedb131abd17ba8e
ysj <tysj1597@gmail.com>
2020-09-17 12:55:49 +0530

    Adding js functions to remove vowels and to find whether a number is a perfect square

---
+++ important_code_snippets.js
@@ -0,0 +1,5 @@
+//js function to remove vowels from a string
+vowelRemover=str=>str.remove('[aeiou]/ig','')
+
+//js function to find whether a number is a perfect square
+perfectSquare=num=>num>=0 && num%1===0
