3442fa155f53b8f3fd6c9d0b790a53bac644e7bc
ysj <tysj1597@gmail.com>
2020-09-18 20:05:28 +0530

    Added function to replace the occurences of a substring in a string

--- important_code_snippets.js
+++ important_code_snippets.js
@@ -3,3 +3,6 @@ vowelRemover=str=>str.remove('[aeiou]/ig','')
 
 //js function to find whether a number is a perfect square
 perfectSquare=num=>num>=0 && Math.sqrt(num)%1===0
+
+//js function to remove the occurences of any substring from the given string
+filteredString=givenString=>givenString.replace(/(substring)+/g,' ').trim() 
