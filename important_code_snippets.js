f39b82f6346a3581599c41043a4c57361f5a9086
ysj <tysj1597@gmail.com>
2020-09-19 19:49:02 +0530

    Beautiful use of reduce on array

--- important_code_snippets.js
+++ important_code_snippets.js
@@ -6,3 +6,7 @@ perfectSquare=num=>num>=0 && Math.sqrt(num)%1===0
 
 //js function to remove the occurences of any substring from the given string
 filteredString=givenString=>givenString.replace(/(substring)+/g,' ').trim() 
+
+//beautiful use of reduce function on arrays having array or tuples as element of the array.
+//accumulator stores the overall value throughout the iteration , number is the element taken on each iteration from the array.
+total=givenArray=>givenArray.reduce((accumulator,number)=>accumulator+number[0]-number[1])
